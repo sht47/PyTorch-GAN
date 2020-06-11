46dee45e803e383d646b7d94ff12d596dc357909
Shintaro Yoshida <yoshidashintaro0920@gmail.com>
2020-06-12 00:32:45 +0900

    [UPD] condition

--- implementations/cogan/cogan.py
+++ implementations/cogan/cogan.py
@@ -25,7 +25,7 @@ os.makedirs("images", exist_ok=True)
 os.makedirs("results", exist_ok=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=500, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=8, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
